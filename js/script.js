1704c0222a90ad2f0ccbe1d2e9d4f2453395c9ae
HectorOik <oikohector@outlook.com>
2024-09-30 17:16:32 +0100

    first 'publishable' commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// set navbar active state
+const navItems = document.querySelectorAll('.links li')
+
+function setActiveNavItem() {
+    const currentPage = window.location.pathname.split('/').pop();
+
+    navItems.forEach(item => {
+        const linkText = item.querySelector('a').getAttribute('href');
+        if (linkText === currentPage) {
+            item.classList.add('active');
+        } else {
+            item.classList.remove('active');
+        }
+    });
+}
+
+window.onload = setActiveNavItem;
+
 document.getElementById("back-to-top").addEventListener("click", function() {
     window.scrollTo({
         top: 0,
